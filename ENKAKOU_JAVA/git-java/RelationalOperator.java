1f5e003ffdd574b70df103e68d1ce95ac4fde023
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-18 00:19:10 +0900

    Update RelationalOperator.java

--- ENKAKOU_JAVA/git-java/RelationalOperator.java
+++ ENKAKOU_JAVA/git-java/RelationalOperator.java
@@ -1,20 +1,16 @@
-//演示关系运算符的使用
-//
-
 public class RelationalOperator { 
 
-	//编写一个main方法
 	public static void main(String[] args) {
 
-		int a = 9;  //老韩提示:  开发中，不可以使用  a, b 
+		int a = 9; 
 		int b = 8;
-		System.out.println(a > b); //T 
-		System.out.println(a >= b);  //T
-		System.out.println(a <= b); //F
-		System.out.println(a < b);//F
-		System.out.println(a == b); //F
-		System.out.println(a != b); //T
-		boolean flag = a > b; //T
+		System.out.println(a > b); 
+		System.out.println(a >= b);  
+		System.out.println(a <= b); 
+		System.out.println(a < b);
+		System.out.println(a == b); 
+		System.out.println(a != b); 
+		boolean flag = a > b; 
 		System.out.println("flag=" + flag);
 	}
 }
