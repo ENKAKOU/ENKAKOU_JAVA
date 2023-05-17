461d868826d53001f447e35e31f7fb7ee0d86af1
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-17 23:56:10 +0900

    Update MethodExercise01.java

--- ENKAKOU_JAVA/git-java/MethodExercise01.java
+++ ENKAKOU_JAVA/git-java/MethodExercise01.java
@@ -21,8 +21,7 @@ public class MethodExercise01{
 }
 
 class AAB{
-	//类型；boolean；名字：isOdd；形参：int num；方法体：判断；
-	
+
 	public boolean isOdd(int num) {
 		/* if(num % 2 != 0) {
 		
@@ -36,8 +35,6 @@ class AAB{
 		return num % 2 != 0; 
 		
 	}
-	
-	//类型；void；名字：print；形参：int row, int col; ；方法体：循环
 	
 	public void print(int row, int col, char c) {
 		for(int i = 0; i < row; i++) {
