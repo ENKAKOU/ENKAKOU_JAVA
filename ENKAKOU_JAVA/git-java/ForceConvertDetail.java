e8272b1c90382ec848696129d57100128b02ced4
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-17 23:46:14 +0900

    Update ForceConvertDetail.java

--- ENKAKOU_JAVA/git-java/ForceConvertDetail.java
+++ ENKAKOU_JAVA/git-java/ForceConvertDetail.java
@@ -1,21 +1,15 @@
-
 public class ForceConvertDetail { 
-
-	//编写一个main方法
 	public static void main(String[] args) {
 		
-		//演示强制类型转换
-		//强转符号只针对于最近的操作数有效，往往会使用小括号提升优先级
-		//int x = (int)10*3.5+6*1.5;//编译错误： double -> int 
-		int x = (int)(10*3.5+6*1.5);// (int)44.0 -> 44
-		System.out.println(x);//44
+		//int x = (int)10*3.5+6*1.5;
+		int x = (int)(10*3.5+6*1.5);
+		System.out.println(x);
 
-
-		char c1 = 100; //ok
-		int m = 100; //ok
-		//char c2 = m; //错误
-		char c3 = (char)m; //ok
-		System.out.println(c3);//100对应的字符, d字符
+		char c1 = 100; 
+		int m = 100;
+		//char c2 = m; 
+		char c3 = (char)m; 
+		System.out.println(c3);
 
 		
 	}
