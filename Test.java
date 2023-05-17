84e1bac2270310a2e94cd2020507484d6d4e9440
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-17 12:46:47 +0900

    hsp_pkg

--- Test.java
+++ Test.java
@@ -1,20 +1,15 @@
-package com.use;
+package com.hspedu.pkg;
 
-import com.xiaoqiang.Dog;
-
-import java.util.Scanner;
-
+import com.hspedu.modifier.A;
 
 public class Test {
     public static void main(String[] args) {
+        A a = new A();
+        //在不同包下，可以访问public 修饰的属性或方法
+        //但是不能访问 protected ,默认，private修饰的属性或方法
+        System.out.println(a.n1);
 
-        Dog dog = new Dog();
-        System.out.println(dog);//com.xiaoqiang.Dog@1540e19d
-
-        com.xiaoming.Dog dog1 = new com.xiaoming.Dog();
-        System.out.println(dog1);//com.xiaoming.Dog@677327b6
-
-
-        System.out.println(Math.abs(-1));
+        a.m1();
+        //不能访问 a.m2() a.m3() a.m4()
     }
 }
