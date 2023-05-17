9e851d9545c7fab30be8a4d75cc3bfdef09b4aab
enkakou <enkakou0716@gmail.com>
2023-05-17 22:27:44 +0900

    null

---
+++ ENKAKOU_JAVA/git-java/ChangeCharExer01.java
@@ -0,0 +1,15 @@
+
+public class ChangeCharExer01 {
+	
+	public static void main(String[] args) {
+		System.out.println("����\t����\t�۸�\t����\n����\t�޹���\t120\t1000");
+
+		
+		int n1 = 10;
+		int n2 = 30;
+	
+		int sum = n1 + n2;
+		
+		System.out.println("���=" + sum);
+	}
+}
