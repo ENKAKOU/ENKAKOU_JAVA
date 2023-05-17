279d0c563657b1764ed1cf134b29d8f18d9045da
enkakou <enkakou0716@gmail.com>
2023-05-17 17:16:19 +0900

    houserent.HouseRentApp

---
+++ ENKAKOU_JAVA/git-java/houserent/HouseRentApp.java
@@ -0,0 +1,18 @@
+package houserent;
+
+import com.hspedu.houserent.view.HouseView;
+
+public class HouseRentApp {
+    public static void main(String[] args) {
+        byte b1 = 123;
+        b1 += 100000;
+        System.out.println(b1);
+
+        byte b2 = 123;
+        b2 = (byte)(b2 + 100000);
+        System.out.println(b2);
+
+        new HouseView().mainMenu();
+        System.out.println("===你推出房屋出租系统===");
+    }
+}
