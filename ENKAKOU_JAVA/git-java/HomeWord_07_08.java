6586a05d79e11b1dcda7dc9b47de9da04c5575cb
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-17 23:47:41 +0900

    Update HomeWord_07_08.java

--- ENKAKOU_JAVA/git-java/HomeWord_07_08.java
+++ ENKAKOU_JAVA/git-java/HomeWord_07_08.java
@@ -1,8 +1,6 @@
 public class HomeWord_07_08 {
-	
-	
     public static void main(String[] args){
-        //求出 1-1/2+1/3-1/4....1/100 的和
+    
         double sum = 0;
         for(int i = 1; i <= 100; i++){
             if( i % 2 != 0){
@@ -15,8 +13,6 @@ public class HomeWord_07_08 {
 
         System.out.println();
 
-        // 例题8
-            /* 求1+（1+2）+（1+2+3）+（1+2+3+4）+....+（1+2+3...100）的结果    */
         int sum2 = 0;
         for(int a = 1; a <= 100; a++){
             for(int b = 1; b <= a; b++){
