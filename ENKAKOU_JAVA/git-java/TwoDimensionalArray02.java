57b8d91f441fac83d031bfa6c8b96c3455ef7748
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-18 00:07:32 +0900

    Update TwoDimensionalArray02.java

--- ENKAKOU_JAVA/git-java/TwoDimensionalArray02.java
+++ ENKAKOU_JAVA/git-java/TwoDimensionalArray02.java
@@ -1,20 +1,16 @@
 public class TwoDimensionalArray02 {
 	
     public static void main(String[] args){
-
-        //1) 语法: 类型[][] 数组名=new 类型[大小][大小]
-        //2) 比如: int a[][]=new int[2][3]
-
         //int arr[][] = new int[2][3];
-        int arr[][];                //声明二维数组
-        arr = new int[2][3];        //再开空间
+        int arr[][];              
+        arr = new int[2][3];     
         arr[1][1] = 8;
-        //遍历 arr 数组
+        //閬嶅巻 arr 鏁扮粍
         for(int i = 0; i < arr.length; i++) {
-            for(int j = 0; j < arr[i].length; j++) {     //对每个一维数组遍历
+            for(int j = 0; j < arr[i].length; j++) {    
                 System.out.print(arr[i][j] +" ");
             }
-            System.out.println();//换行
+            System.out.println();
         }
 
 
