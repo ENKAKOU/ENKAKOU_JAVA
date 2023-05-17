f1b130b3ecdbcfe2aa15aab03cac887311b8fde6
enkakou <enkakou0716@gmail.com>
2023-05-17 21:59:25 +0900

    houserent.utils

---
+++ ENKAKOU_JAVA/git-java/houserent/utils/TestUtility.java
@@ -0,0 +1,22 @@
+package houserent.utils;
+
+import com.hspedu.houserent.utils.Utility;
+
+public class TestUtility {
+    public static void main(String[] args) {
+        System.out.println("请输入字符串");
+        String s2 = Utility.readString(10, "hspedu");
+        System.out.println("s2=" + s2);
+
+        A.cry();
+    }
+}
+
+class A {
+    public void hi(){
+
+    }
+    public static void cry() {
+
+    }
+}
