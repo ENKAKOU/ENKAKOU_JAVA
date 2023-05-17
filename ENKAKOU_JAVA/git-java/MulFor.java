89dc4a7c87b938dcaacdfa3019973f91aa633c44
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-17 23:57:50 +0900

    Update MulFor.java

--- ENKAKOU_JAVA/git-java/MulFor.java
+++ ENKAKOU_JAVA/git-java/MulFor.java
@@ -1,10 +1,8 @@
-
 public class MulFor { 
-
-	//编写一个main方法
+	
 	public static void main(String[] args) {
 
-		for(int i = 0; i < 2; i++) { //先思考
+		for(int i = 0; i < 2; i++) { 
 		    for( int j = 0; j < 3; j++) { 
 		        System.out.println("i=" + i + "j=" + j);
 		    }
