27a1a5f54eaa6ccefebea5e5e09bd28bdfc508de
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-17 23:41:22 +0900

    Update ConstructorDetail.java

--- ENKAKOU_JAVA/git-java/ConstructorDetail.java
+++ ENKAKOU_JAVA/git-java/ConstructorDetail.java
@@ -1,7 +1,4 @@
-
 public class ConstructorDetail { 
-
-
 	public static void main(String[] args) {
 		Person p1 = new Person("king", 40);
 		Person p2 = new Person("tom");
@@ -11,18 +8,7 @@ public class ConstructorDetail {
 	}
 }
 class Dog {
-	//如果程序员没有定义构造器，系统会自动给类生成一个默认无参构造器(也叫默认构造器)
-    //使用 javap 指令 反编译看看
-    /*
-    默认构造器
-    Dog() {
-    }
-    */
-    //一旦定义了自己的构造器,默认的构造器就覆盖了，就不能再使用默认的无参构造器，
-    //除非显式的定义一下,即: Dog(){} 写 (这点很重要)
-    //
 	public Dog(String dName) {
-		//...
 	}
 	Dog() {
 
