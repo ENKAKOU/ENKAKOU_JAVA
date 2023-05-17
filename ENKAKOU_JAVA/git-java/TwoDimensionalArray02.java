e23dfb4e2bf1bc242abdc18de837f8a406cb09d0
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-18 00:20:08 +0900

    Update TwoDimensionalArray02.java

--- ENKAKOU_JAVA/git-java/TwoDimensionalArray02.java
+++ ENKAKOU_JAVA/git-java/TwoDimensionalArray02.java
@@ -5,7 +5,7 @@ public class TwoDimensionalArray02 {
         int arr[][];              
         arr = new int[2][3];     
         arr[1][1] = 8;
-        //遍历 arr 数组
+
         for(int i = 0; i < arr.length; i++) {
             for(int j = 0; j < arr[i].length; j++) {    
                 System.out.print(arr[i][j] +" ");
