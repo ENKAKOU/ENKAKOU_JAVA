b9e46c5115f16c86d2573152b6bff805b6606c38
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-17 23:44:28 +0900

    Update For01.java

--- ENKAKOU_JAVA/git-java/For01.java
+++ ENKAKOU_JAVA/git-java/For01.java
@@ -1,24 +1,21 @@
-
 public class For01 { 
 
-	//编写一个main方法
 	public static void main(String[] args) {
-		//打印10句 "你好，韩顺平教育!"
-		//传统的方法
-		// System.out.println("你好，韩顺平教育");
-		// System.out.println("你好，韩顺平教育");
-		// System.out.println("你好，韩顺平教育");
-		// System.out.println("你好，韩顺平教育");
-		// System.out.println("你好，韩顺平教育");
-		// System.out.println("你好，韩顺平教育");
-		// System.out.println("你好，韩顺平教育");
-		// System.out.println("你好，韩顺平教育");
-		// System.out.println("你好，韩顺平教育");
-		// System.out.println("你好，韩顺平教育");
-		// 
-		// 使用for循环控制
+		
+		// System.out.println("浣犲ソ锛岄煩椤哄钩鏁欒偛");
+		// System.out.println("浣犲ソ锛岄煩椤哄钩鏁欒偛");
+		// System.out.println("浣犲ソ锛岄煩椤哄钩鏁欒偛");
+		// System.out.println("浣犲ソ锛岄煩椤哄钩鏁欒偛");
+		// System.out.println("浣犲ソ锛岄煩椤哄钩鏁欒偛");
+		// System.out.println("浣犲ソ锛岄煩椤哄钩鏁欒偛");
+		// System.out.println("浣犲ソ锛岄煩椤哄钩鏁欒偛");
+		// System.out.println("浣犲ソ锛岄煩椤哄钩鏁欒偛");
+		// System.out.println("浣犲ソ锛岄煩椤哄钩鏁欒偛");
+		// System.out.println("浣犲ソ锛岄煩椤哄钩鏁欒偛");
+	 
+		// 浣跨敤for寰幆鎺у埗
 		for( int i = 1; i <= 10; i++) {
-			System.out.println("你好，韩顺平教育" + i);
+			System.out.println("浣犲ソ锛岄煩椤哄钩鏁欒偛" + i);
 		}
 		
 	}
