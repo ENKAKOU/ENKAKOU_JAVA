28a88ad3309eeacf06e442d199f554f10566728a
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-17 23:21:34 +0900

    Update ArithmeticOperator.java

--- ENKAKOU_JAVA/git-java/ArithmeticOperator.java
+++ ENKAKOU_JAVA/git-java/ArithmeticOperator.java
@@ -3,37 +3,29 @@ public class ArithmeticOperator {
 	
 	public static void main(String[] args) {
 	
-		System.out.println(10 / 4); //从数学来看是2.5, java中 2
-		System.out.println(10.0 / 4); //java是2.5
-		
-		double d = 10 / 4;//java中10 / 4 = 2, 2=>2.0 
-		System.out.println(d);// 是2.0
+		System.out.println(10 / 4); 
+		System.out.println(10.0 / 4); 
+		double d = 10 / 4;
+		System.out.println(d);
 
-		
 		// -10 % 3 => -10 - (-10) / 3 * 3 = -10 + 9 = -1
 		// 10 % -3 = 10 - 10 / (-3) * (-3) = 10 - 9 = 1
 		// -10 % -3 =  (-10) - (-10) / (-3) * (-3) = -10 + 9 = -1
-		System.out.println(10 % 3); //1
+		System.out.println(10 % 3); 
 
-		System.out.println(-10 % 3); // -1
-		System.out.println(10 % -3); //1
-		System.out.println(-10 % -3);//-1
+		System.out.println(-10 % 3);
+		System.out.println(10 % -3); 
+		System.out.println(-10 % -3);
 
-		//++的使用
-		//
+		
 		int i = 10;
-		i++;//自增 等价于 i = i + 1; => i = 11
-		++i;//自增 等价于 i = i + 1; => i = 12
-		System.out.println("i=" + i);//12
-
-		/*
-		作为表达式使用
-        前++：++i先自增后赋值
-        后++：i++先赋值后自增
-		 */
+		i++;  //自增 等价于 i = i + 1; => i = 11
+		++i;  //自增 等价于 i = i + 1; => i = 12
+		System.out.println("i=" + i);
+		
 		int j = 8;
-		//int k = ++j; //等价 j=j+1;k=j; 
-		int k = j++; // 等价 k =j;j=j+1;
+		//int k = ++j;
+		int k = j++; 
 		System.out.println("k=" + k + "j=" + j);//8 9
 	}
 }
