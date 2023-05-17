e97558138675b20e4ae2f577ef6b04af0db37a84
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-17 23:42:09 +0900

    Update ContinueDetail.java

--- ENKAKOU_JAVA/git-java/ContinueDetail.java
+++ ENKAKOU_JAVA/git-java/ContinueDetail.java
@@ -1,7 +1,5 @@
-
 public class ContinueDetail { 
 
-	//编写一个main方法
 	public static void main(String[] args) {
 
 		label1:
@@ -9,12 +7,11 @@ public class ContinueDetail {
 			label2:
 			for(int i = 0; i < 10; i++){
 				if(i == 2){
-					//看看分别输出什么值，并分析
-					//continue ; //等价于 continue label2
-					//continue label2;//等价 continue;
-					continue label1; //输出 2次[0,1]
+					//continue ; 
+					//continue label2;
+					continue label1; 
 				}
-				System.out.println("i = " + i);//输出2次[0,1,3,4,5,6,7,8,9]
+				System.out.println("i = " + i);
 	        }
 	     }
 
