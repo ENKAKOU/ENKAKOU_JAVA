a2402cea0e484fea8198af4e556bb1c09d93e770
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-17 23:29:31 +0900

    Update ArrayStack.java

--- ENKAKOU_JAVA/git-java/ArrayStack.java
+++ ENKAKOU_JAVA/git-java/ArrayStack.java
@@ -4,21 +4,18 @@ public class ArrayStack {
 	}
 	
 
-    private int size;         // 数组大小
-    private int[] stack;      // 数组
-    private int top = -1;     // 栈顶在数组为空时，值为-1
+    private int size;
+    private int[] stack;     
+    private int top = -1;    
  
     public ArrayStack(int initialSize) {
-        // 保证数组大小为正
-    	
         if (initialSize <= 0)
             throw new RuntimeException("The size should be positive");
         
         this.size = initialSize;     
         stack = new int[initialSize];
     }
- 
-    // // 通过栈顶所在位置判断数组是否已满或为空
+
     public boolean isFull() { return top == size - 1; }   
  
     public boolean isEmpty() {
@@ -27,7 +24,6 @@ public class ArrayStack {
  
     public void push(int value) {
     	
-        // 如果数组已满，先调整大小
         if (isFull()) {
             resize();
         }
@@ -57,11 +53,8 @@ public class ArrayStack {
     }
  
     public void resize() {
-        // 定义一个大小为原数组两倍的新数组
-    	
+      
         int[] newArray = new int[size * 2];
- 
-        // 复制原数组的数据至新数组
         for (int i=0; i<size; i++)
             newArray[i] = stack[i];
         stack = newArray;
