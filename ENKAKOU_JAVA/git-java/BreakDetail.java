cb05c5291fc41c52fb56060ea80a84ac7440da22
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-17 23:36:07 +0900

    Update BreakDetail.java

--- ENKAKOU_JAVA/git-java/BreakDetail.java
+++ ENKAKOU_JAVA/git-java/BreakDetail.java
@@ -1,16 +1,12 @@
-
-
 public class BreakDetail { 
-
-	//编写一个main方法
 	public static void main(String[] args) {
 
 		abc1:
-		for(int j = 0; j < 4; j++){//外层for
+		for(int j = 0; j < 4; j++){
 		abc2:
-			for(int i = 0; i < 10; i++){//内层for
+			for(int i = 0; i < 10; i++){
 				if(i == 2){
-					//break  ;//等价 break abc2
+					//break  
 					break abc1 ;
 				}
 				System.out.println("i = " + i);
