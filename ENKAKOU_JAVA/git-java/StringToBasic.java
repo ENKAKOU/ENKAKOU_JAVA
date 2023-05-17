a832155a6f7327541acee113b990522250e99987
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-18 00:03:23 +0900

    Update StringToBasic.java

--- ENKAKOU_JAVA/git-java/StringToBasic.java
+++ ENKAKOU_JAVA/git-java/StringToBasic.java
@@ -1,10 +1,6 @@
-
 public class StringToBasic { 
 
-	//编写一个main方法
 	public static void main(String[] args) {
-		
-		//基本数据类型->String
 		
 		int n1 = 100;
 		float f1 = 1.1F;
@@ -16,10 +12,7 @@ public class StringToBasic {
 		String s4 = b1 + "";
 		System.out.println(s1 + " " + s2 + " " + s3 + " " + s4);
 
-		//String->对应的基本数据类型
 		String s5 = "123";
-		//会在OOP 讲对象和方法的时候回详细
-		//解读 使用 基本数据类型对应的包装类，的相应方法，得到基本数据类型
 		int num1 = Integer.parseInt(s5);
 		double num2 = Double.parseDouble(s5);
 		float num3 = Float.parseFloat(s5);
@@ -37,8 +30,6 @@ public class StringToBasic {
 		System.out.println(num6);//123
 		System.out.println(b);//true
 
-		//怎么把字符串转成字符char -> 含义是指 把字符串的第一个字符得到
-		//解读  s5.charAt(0) 得到 s5字符串的第一个字符 '1'
 		System.out.println(s5.charAt(0));
 		
 	}
