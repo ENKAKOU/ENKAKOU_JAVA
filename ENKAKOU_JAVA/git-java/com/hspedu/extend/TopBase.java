dc6c426acf0400618e6dccd513a003f2d765821b
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-18 00:23:05 +0900

    Update TopBase.java

--- ENKAKOU_JAVA/git-java/com/hspedu/extend/TopBase.java
+++ ENKAKOU_JAVA/git-java/com/hspedu/extend/TopBase.java
@@ -1,7 +1,7 @@
 package com.hspedu.extend_;
-public class TopBase { //父类是 Object
+public class TopBase {
     public TopBase() {
 //  super(); Object 的无参构造器
-        System.out.println("构造器 TopBase() 被调用...");//1
+        System.out.println("构造器 TopBase() 被调用...");
     }
 }
