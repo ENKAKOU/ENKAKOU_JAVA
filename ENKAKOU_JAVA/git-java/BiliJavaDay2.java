9539695e82d9f3f238d75843150f94f82cc65f4f
enkakou <enkakou0716@gmail.com>
2023-05-19 17:49:42 +0900

    Modify missing data

---
+++ ENKAKOU_JAVA/git-java/BiliJavaDay2.java
@@ -0,0 +1,31 @@
+public class BiliJavaDay2 {
+
+    public static void main(String[] args){
+
+        int n1 = 1 + 2;
+        int n2 = 5 + 10;
+        int n3 = 1 + 3 + 34;
+        System.out.println(n1 + n2);
+        System.out.println("hello world");
+
+        int a = 1;
+        System.out.println(a);
+        a = 100;
+        System.out.println(a);
+        int b = 100;
+        System.out.println(b);
+
+        int age = 20;
+        double score = 88.6;
+        char gender = '女';
+        String name = "jack";
+
+        System.out.println("信息如下");
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(score);
+        System.out.println(gender);
+
+
+    }
+}
