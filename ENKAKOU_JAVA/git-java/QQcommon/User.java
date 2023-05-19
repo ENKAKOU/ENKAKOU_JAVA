ff033460e5643c356c5c6138ed526233102d80ab
enkakou <enkakou0716@gmail.com>
2023-05-19 22:41:29 +0900

    qqcommon

--- ENKAKOU_JAVA/git-java/QQcommon/User.java
+++ ENKAKOU_JAVA/git-java/QQcommon/User.java
@@ -1,4 +1,32 @@
 package QQcommon;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private String userId;     //用户Id/用户名
+    private String passwd;     //用户密码
+
+    public User() {}
+    public User(String userId, String passwd) {
+        this.userId = userId;
+        this.passwd = passwd;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
 }
