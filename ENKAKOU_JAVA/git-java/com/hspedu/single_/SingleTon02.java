033343c5b0e9e860c82edb3c8611b984dec0b11c
enkakou <enkakou0716@gmail.com>
2023-05-20 14:09:25 +0900

    single_

---
+++ ENKAKOU_JAVA/git-java/com/hspedu/single_/SingleTon02.java
@@ -0,0 +1,44 @@
+package com.hspedu.single_;
+
+import sun.dc.pr.PRError;
+
+public class SingleTon02 {
+
+    public static void main(String[] args) {
+//      new Cat("大黄");
+//      System.out.println(Cat.n1);
+        Cat instance = Cat.getInstance();
+        System.out.println(instance);
+
+        Cat instance2 = Cat.getInstance();
+        System.out.println(instance2);
+
+        System.out.println(instance == instance2);
+
+    }
+}
+
+class Cat {
+    private String name;
+    public static  int n1 = 999;
+    private static Cat cat ;
+
+    private Cat(String name){
+        System.out.println("构造器调用...");
+        this.name = name;
+    }
+
+    public static Cat getInstance() {
+        if (cat == null) {
+            cat = new Cat("小可爱");
+        }
+        return cat;
+    }
+
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                '}';
+
+    }
+}
