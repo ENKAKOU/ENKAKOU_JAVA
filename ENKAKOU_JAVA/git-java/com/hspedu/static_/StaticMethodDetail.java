4bf076cebe83bc9baae8af13c001b63e64b3755b
enkakou <enkakou0716@gmail.com>
2023-05-20 14:42:25 +0900

    static_

---
+++ ENKAKOU_JAVA/git-java/com/hspedu/static_/StaticMethodDetail.java
@@ -0,0 +1,37 @@
+package com.hspedu.static_;
+
+public class StaticMethodDetail {
+
+    public static void main(String[] args) {
+        D.hi();
+        new D().say();
+    }
+
+}
+
+class D {
+    private int n1 = 100;
+    private static  int n2 = 200;
+
+    public void say() {
+    }
+
+    public static  void hi() {
+        //System.out.println(this.n1);
+    }
+
+    public static void hello() {
+        System.out.println(n2);
+        System.out.println(D.n2);
+        //System.out.println(this.n2);
+        hi();
+    }
+
+    public void ok() {
+        System.out.println(n1);
+        say();
+        System.out.println(n2);
+        hello();
+
+    }
+}
