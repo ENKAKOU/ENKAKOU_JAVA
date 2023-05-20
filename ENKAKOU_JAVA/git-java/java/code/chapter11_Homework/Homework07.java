7efefd338afe505abec3a5e9d8728d1a4a35457d
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-20 15:00:35 +0900

    Update Homework07.java

--- ENKAKOU_JAVA/git-java/java/code/chapter11_Homework/Homework07.java
+++ ENKAKOU_JAVA/git-java/java/code/chapter11_Homework/Homework07.java
@@ -1,12 +1,9 @@
 package com.hspedu.homework;
 
-/**
- * @author 韩顺平
- * @version 1.0
- */
 public class Homework07 {
+    
     public static void main(String[] args) {
-        //实例化不同的car对象
+      
         Car2 car2 = new Car2(60);
         car2.getAir().flow();
         Car2 car21 = new Car2(-1);
@@ -15,22 +12,14 @@ public class Homework07 {
         car22.getAir().flow();
     }
 }
-/*
-有一个Car2类，有属性temperature（温度），车内有Air（空调）类，有吹风的功能flow，
-Air会监视车内的温度，如果温度超过40度则吹冷气。如果温度低于0度则吹暖气，
-如果在这之间则关掉空调。实例化具有不同温度的Car对象，调用空调的flow方法，
-测试空调吹的风是否正确 . //体现 类与类的包含关系的案例 类(内部类【成员内部类】)
 
- */
 class Car2 {
-
    private double temperature;
 
     public Car2(double temperature) {
         this.temperature = temperature;
     }
 
-    //Air 成员内部类
    class Air {
        public void flow() {
            if(temperature > 40) {
@@ -42,8 +31,9 @@ class Car2 {
            }
        }
    }
-   //返回一个Air对象
+
     public Air getAir() {
        return new Air();
+        
     }
 }
