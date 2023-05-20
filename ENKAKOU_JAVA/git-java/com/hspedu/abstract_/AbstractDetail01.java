c881b83747664bb3dd01b31a0d15c001f6ded4fd
enkakou <enkakou0716@gmail.com>
2023-05-20 11:27:47 +0900

    hspedu.abstract

---
+++ ENKAKOU_JAVA/git-java/com/hspedu/abstract_/AbstractDetail01.java
@@ -0,0 +1,22 @@
+package com.hspedu.abstract_;
+
+public class AbstractDetail01 {
+
+    public static void main(String[] args) {
+        //new A();
+    }
+}
+
+abstract class A {
+    public void h1() {
+        System.out.println("hi");
+    }
+}
+
+abstract class B {
+    public abstract void h1();
+}
+
+class C {
+    // public abstract int n1 = 1;
+}
---
+++ ENKAKOU_JAVA/git-java/com/hspedu/abstract_/AbstractDetail02.java
@@ -0,0 +1,37 @@
+package com.hspedu.abstract_;
+
+public class AbstractDetail02 {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
+
+abstract class H {
+    public abstract void h1();
+}
+
+abstract class E {
+    public abstract void h1();
+}
+
+abstract class F extends E {
+}
+
+class G extends E {
+    public void h1() {
+
+    }
+}
+
+abstract class D {
+    public int n1 = 10;
+    public static String name = "HSP教育";
+    public void h1() {
+        System.out.println("h1");
+    }
+    public abstract void hello();
+    public static void ok() {
+        System.out.println("ok");
+    }
+}
