7bb6bbcdbf39e876c2f002ec9555d425c6ddbec9
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-20 15:01:07 +0900

    Update Homework08.java

--- ENKAKOU_JAVA/git-java/java/code/chapter11_Homework/Homework08.java
+++ ENKAKOU_JAVA/git-java/java/code/chapter11_Homework/Homework08.java
@@ -1,17 +1,11 @@
 package com.hspedu.homework;
 
-/**
- * @author 韩顺平
- * @version 1.0
- */
 public class Homework08 {
+    
     public static void main(String[] args) {
-        //演示一下枚举值得switch使用
         Color green = Color.GREEN;
         green.show();
-        //比较一下
-        //switch () 中，放入枚举对象
-        //在每个case 后，直接写上在枚举类中，定义的枚举对象即可
+      
         switch (green) {
             case YELLOW:
                 System.out.println("匹配到黄色");
@@ -25,19 +19,6 @@ public class Homework08 {
     }
 }
 
-/*
-枚举类
-创建一个Color枚举类
-1.有 RED,BLUE,BLACK,YELLOW,GREEN这个五个枚举值/对象；
-2.Color有三个属性redValue，greenValue，blueValue，
-3.创建构造方法，参数包括这三个属性，
-4.每个枚举值都要给这三个属性赋值，三个属性对应的值分别是
-red：255,0,0  blue:0,0,255  black:0,0,0  yellow:255,255,0  green:0,255,0
-5.定义接口，里面有方法show，要求Color实现该接口
-6.show方法中显示三属性的值
-7. 将枚举对象在switch语句中匹配使用
-
- */
 interface IMyInterface {
     public void show();
 }
@@ -54,8 +35,8 @@ enum Color implements IMyInterface {
         this.blueValue = blueValue;
     }
 
-    @Override
     public void show() {
         System.out.println("属性值为" + redValue + "," + greenValue + "," + blueValue);
+        
     }
 }
