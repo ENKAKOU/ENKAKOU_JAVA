5364d6906815bc019c37c62224dccf9c1b35202a
enkakou <enkakou0716@gmail.com>
2023-05-20 16:05:55 +0900

    annotation

---
+++ ENKAKOU_JAVA/git-java/com/hspedu/annotation_/Deprecated_.java
@@ -0,0 +1,18 @@
+package com.hspedu.annotation_;
+
+public class Deprecated_ {
+
+    public static void main(String[] args) {
+        A a =new A();
+        a.h1();
+        System.out.println(a.n1);
+    }
+}
+
+class A {
+    public int n1 = 10;
+
+    public void h1() {
+
+    }
+}
