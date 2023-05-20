94402f04609e8b3fe0412b16185649545c4df81b
enkakou <enkakou0716@gmail.com>
2023-05-20 14:09:12 +0900

    codeblock

---
+++ ENKAKOU_JAVA/git-java/com/hspedu/codeblock_/CodeBlock01.java
@@ -0,0 +1,44 @@
+package com.hspedu.codeblock_;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+public class CodeBlock01 {
+
+    public static void main(String[] args) {
+
+        Movie movie = new Movie("你好，李焕英");
+        System.out.println("===========");
+        Movie movie2 = new Movie("唐人街3", 100, "陈锡城");
+    }
+}
+
+class Movie {
+    private String name;
+    private double price;
+    private String director;
+
+    {
+        System.out.println("电影屏幕打开...");
+        System.out.println("广告开始...");
+        System.out.println("电影正是开始...");
+    }
+
+    public Movie(String name) {
+        System.out.println("Movie(String name) 被调用...");
+        this.name = name;
+    }
+
+    public Movie(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Movie(String name, double price, String director) {
+        System.out.println("Movie(String name, double price, String director) 被调用...");
+        this.name = name;
+        this.price = price;
+        this.director = director;
+
+    }
+}
---
+++ ENKAKOU_JAVA/git-java/com/hspedu/codeblock_/CodeBlockDetail02.java
@@ -0,0 +1,37 @@
+package com.hspedu.codeblock_;
+
+public class CodeBlockDetail02 {
+
+    public static void main(String[] args) {
+        A a = new A();
+    }
+}
+
+class A {
+    {
+        System.out.println("A 普通代码块01");
+    }
+
+    private int n2 = getN2();
+
+    static {
+        System.out.println("A 静态代码块01");
+    }
+
+    private static  int n1 = getN1();
+
+    public static int getN1() {
+        System.out.println("getN1被调用...");
+        return 100;
+    }
+
+    public int getN2() {
+        System.out.println("getN2被调用...");
+        return 200;
+    }
+
+    public A() {
+        System.out.println("A() 构造器被调用");
+
+    }
+}
