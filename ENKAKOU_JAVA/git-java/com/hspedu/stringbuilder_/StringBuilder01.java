0744ee113fe4237544a6756c08d6b0c77402846c
enkakou <enkakou0716@gmail.com>
2023-05-22 16:56:20 +0900

    stringbuilder_

---
+++ ENKAKOU_JAVA/git-java/com/hspedu/stringbuilder_/StringBuilder01.java
@@ -0,0 +1,9 @@
+package com.hspedu.stringbuilder_;
+
+public class StringBuilder01 {
+
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+    }
+}
