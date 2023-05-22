554e8d173cdd1862eb8257f2b306e12c5a6ceace
enkakou <enkakou0716@gmail.com>
2023-05-22 16:39:35 +0900

    wrapper

---
+++ ENKAKOU_JAVA/git-java/com/hspedu/wrapper/WrapperType.java
@@ -0,0 +1,18 @@
+package com.hspedu.wrapper;
+
+public class WrapperType {
+
+    public static void main(String[] args) {
+
+//        boolean -> Boolean
+//        char -> Character
+//        byte -> Byte
+//        int -> Integer
+//        long -> long
+//        float -> Float
+//        double -> Double
+//        short -> Short
+
+    }
+}
+
