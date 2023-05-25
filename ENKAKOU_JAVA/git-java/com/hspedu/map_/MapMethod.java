c9bc547e726797f5544bfcae21a8c709c36e4e22
enkakou <enkakou0716@gmail.com>
2023-05-25 21:23:44 +0900

    com.hspedu.map_

---
+++ ENKAKOU_JAVA/git-java/com/hspedu/map_/MapMethod.java
@@ -0,0 +1,46 @@
+package com.hspedu.map_;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMethod {
+
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put("邓超", new Book("", 100));
+        map.put("邓超", "孙俪");
+        map.put("王宝强", "马蓉");
+        map.put("宋喆", "马蓉");
+        map.put("刘令博", null);
+        map.put(null, "刘亦菲");
+        map.put("鹿晗", "关晓彤");
+        map.put("hsp", "hsp的老婆");
+
+        System.out.println("map=" + map);
+
+        map.remove(null);
+        System.out.println("map=" + map);
+
+        Object val = map.get("鹿晗");
+        System.out.println("val=" + val);
+
+        System.out.println("k-v=" + map.size());
+        System.out.println(map.isEmpty());
+        //map.clear();
+        System.out.println("map=" + map);
+        System.out.println("结果=" + map.containsKey("hsp"));
+
+    }
+}
+
+
+class Book {
+    private String name;
+    private int num;
+
+    public Book(String name, int num) {
+        this.name = name;
+        this.num = num;
+
+    }
+}
---
+++ ENKAKOU_JAVA/git-java/com/hspedu/map_/MapSource_.java
@@ -0,0 +1,37 @@
+package com.hspedu.map_;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MapSource_ {
+
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put("no1", "韩顺平");
+        map.put("no2", "张无忌");
+        map.put(new Car(), new Person());
+
+        Set set = map.entrySet();
+        System.out.println(set.getClass());
+        for (Object obj : set) {
+            Map.Entry entry = (Map.Entry) obj;
+            System.out.println(entry.getKey() + "-" + entry.getValue() );
+        }
+
+        Set set1 = map.keySet();
+        System.out.println(set1.getClass());
+        Collection values = map.values();
+        System.out.println(values.getClass());
+
+    }
+}
+
+class Car {
+
+}
+
+class Person{
+
+}
