4ce110472bf3b20df3f38b9cfd62c4954137ca65
enkakou <enkakou0716@gmail.com>
2023-05-25 17:14:30 +0900

    java_code_chapter14_Homework

---
+++ ENKAKOU_JAVA/git-java/java/code/chapter14_Homework/Homework04.java
@@ -0,0 +1,18 @@
+package java.code.chapter14_Homework;
+
+import java.util.TreeSet;
+@SuppressWarnings({"all"})
+
+public class Homework04 {
+
+    public static void main(String[] args) {
+
+    TreeSet treeSet = new TreeSet();
+    treeSet.add("hsp");
+    treeSet.add("tom");
+    treeSet.add("king");
+    treeSet.add("hsp");
+    System.out.println(treeSet);
+
+    }
+}
