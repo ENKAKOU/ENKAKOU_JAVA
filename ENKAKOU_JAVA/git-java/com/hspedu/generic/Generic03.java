b64a9866412cc325365ec3da67bd27e6a9413aa5
enkakou <enkakou0716@gmail.com>
2023-05-25 22:09:40 +0900

    com.hspedu.generic

---
+++ ENKAKOU_JAVA/git-java/com/hspedu/generic/Generic03.java
@@ -0,0 +1,57 @@
+package com.hspedu.generic;
+
+import java.util.List;
+
+public class Generic03 {
+
+    public static void main(String[] args) {
+        Person<String> person = new Person<String>("韩顺平教育");
+        person.show();
+
+//            class Person {
+//                String s ;
+//
+//                public Person(String s) {
+//                    this.s = s;
+//                }
+//
+//                public String f() {
+//                    return s;
+//                }
+//            }
+
+        Person<Integer> person2 = new Person<Integer>(100);
+        person2.show();
+
+//            class Person {
+//                Integer s ;
+//
+//                public Person(Integer s) {
+//                    this.s = s;
+//                }
+//
+//                public Integer f() {
+//                    return s;
+//                }
+//            }
+
+    }
+}
+
+
+class Person<E> {
+    E s ;
+
+    public Person(E s) {
+        this.s = s;
+    }
+
+    public E f() {
+        return s;
+    }
+
+    public void show() {
+        System.out.println(s.getClass());
+
+    }
+}
---
+++ ENKAKOU_JAVA/git-java/com/hspedu/generic/GenericDetail.java
@@ -0,0 +1,80 @@
+package com.hspedu.generic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericDetail {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<Integer>();
+        //List<int> list2 = new ArrayList<int>();
+
+        Pig<A> aPig = new Pig<A>(new A());
+        aPig.f();
+        Pig<A> aPig2 = new Pig<A>(new B());
+        aPig2.f();
+
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        List<Integer> list2 = new ArrayList<Integer>();
+
+        ArrayList<Integer> list3 = new ArrayList<>();
+        List<Integer> list4 = new ArrayList<>();
+        ArrayList<Pig> pigs = new ArrayList<>();
+
+        ArrayList arrayList = new ArrayList();
+
+//            public boolean add(Object e) {
+//                ensureCapacityInternal(size + 1);  // Increments modCount!!
+//                elementData[size++] = e;
+//                return true;
+//            }
+
+        Tiger tiger = new Tiger();
+
+//            class Tiger {//类
+//                Object e;
+//
+//                public Tiger() {}
+//
+//                public Tiger(Object e) {
+//                    this.e = e;
+//                }
+//            }
+
+    }
+}
+
+
+class Tiger<E> {
+    E e;
+
+    public Tiger() {
+
+    }
+
+    public Tiger(E e) {
+        this.e = e;
+    }
+}
+
+class A {
+
+}
+
+class B extends A {
+
+}
+
+class Pig<E> {
+    E e;
+
+    public Pig(E e) {
+        this.e = e;
+    }
+
+    public void f() {
+        System.out.println(e.getClass());
+
+    }
+}
