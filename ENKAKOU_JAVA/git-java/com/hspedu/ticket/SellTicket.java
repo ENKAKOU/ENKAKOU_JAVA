aeb3e75421c31726e9fee8234977c99dd22d556c
enkakou <enkakou0716@gmail.com>
2023-05-28 14:48:55 +0900

    ticket

---
+++ ENKAKOU_JAVA/git-java/com/hspedu/ticket/SellTicket.java
@@ -0,0 +1,69 @@
+package com.hspedu.ticket;
+
+public class SellTicket {
+
+    public static void main(String[] args) {
+//        SellTicket01 sellTicket01 = new SellTicket01();
+//        SellTicket01 sellTicket02 = new SellTicket01();
+//        SellTicket01 sellTicket03 = new SellTicket01();
+//
+//        sellTicket01.start();//启动售票线程
+//        sellTicket02.start();//启动售票线程
+//        sellTicket03.start();//启动售票线程
+
+        System.out.println("===使用实现接口方式来售票=====");
+        SellTicket02 sellTicket02 = new SellTicket02();
+
+        new Thread(sellTicket02).start();
+        new Thread(sellTicket02).start();
+        new Thread(sellTicket02).start();
+
+    }
+}
+
+
+class SellTicket01 extends Thread {
+    private static int ticketNum = 100;
+
+    public void run() {
+        while (true) {
+
+            if (ticketNum <= 0) {
+                System.out.println("售票结束...");
+                break;
+            }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("窗口 " + Thread.currentThread().getName() + " 售出一张票"
+                    + " 剩余票数=" + (--ticketNum));
+
+        }
+    }
+}
+
+
+class SellTicket02 implements Runnable {
+    private int ticketNum = 100;
+
+    public void run() {
+        while (true) {
+            if (ticketNum <= 0) {
+                System.out.println("售票结束...");
+                break;
+            }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("窗口 " + Thread.currentThread().getName() + " 售出一张票"
+                    + " 剩余票数=" + (--ticketNum));
+
+        }
+    }
+}
