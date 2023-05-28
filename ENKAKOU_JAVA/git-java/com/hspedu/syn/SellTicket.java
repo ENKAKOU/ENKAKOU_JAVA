beaf6af8775c0e254bc43b040a58da0e13032ef5
enkakou <enkakou0716@gmail.com>
2023-05-28 14:48:45 +0900

    syn

---
+++ ENKAKOU_JAVA/git-java/com/hspedu/syn/SellTicket.java
@@ -0,0 +1,134 @@
+package com.hspedu.syn;
+
+public class SellTicket {
+
+    public static void main(String[] args) {
+
+//        SellTicket01 sellTicket01 = new SellTicket01();
+//        SellTicket01 sellTicket02 = new SellTicket01();
+//        SellTicket01 sellTicket03 = new SellTicket01();
+
+//        sellTicket01.start();
+//        sellTicket02.start();
+//        sellTicket03.start();
+
+//        System.out.println("===使用实现接口方式来售票=====");
+//        SellTicket02 sellTicket02 = new SellTicket02();
+//
+//        new Thread(sellTicket02).start();
+//        new Thread(sellTicket02).start();
+//        new Thread(sellTicket02).start();
+
+        SellTicket03 sellTicket03 = new SellTicket03();
+        new Thread(sellTicket03).start();
+        new Thread(sellTicket03).start();
+        new Thread(sellTicket03).start();
+
+    }
+}
+
+
+class SellTicket03 implements Runnable {
+    private int ticketNum = 100;
+    private boolean loop = true;
+    Object object = new Object();
+
+//     public synchronized static void m1() {
+//
+//        synchronized (SellTicket03.class) {
+//            System.out.println("m2");
+//        }
+
+    public synchronized static void m1() {
+
+    }
+
+    public static  void m2() {
+        synchronized (SellTicket03.class) {
+            System.out.println("m2");
+        }
+    }
+
+    public void sell() {
+
+        synchronized (object) {
+            if (ticketNum <= 0) {
+                System.out.println("售票结束...");
+                loop = false;
+                return;
+            }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("窗口 " + Thread.currentThread().getName() + " 售出一张票"
+                    + " 剩余票数=" + (--ticketNum));//1 - 0 - -1  - -2
+
+        }
+    }
+
+    public void run() {
+        while (loop) {
+
+            sell();
+        }
+    }
+}
+
+// new SellTicket01().start()
+// new SellTicket01().start();
+
+class SellTicket01 extends Thread {
+    private static int ticketNum = 100;
+
+//    public void m1() {
+//        synchronized (this) {
+//            System.out.println("hello");
+//        }
+//    }
+
+    public void run() {
+        while (true) {
+
+            if (ticketNum <= 0) {
+                System.out.println("售票结束...");
+                break;
+            }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("窗口 " + Thread.currentThread().getName() + " 售出一张票"
+                    + " 剩余票数=" + (--ticketNum));
+
+        }
+    }
+}
+
+
+class SellTicket02 implements Runnable {
+    private int ticketNum = 100;
+
+    public void run() {
+        while (true) {
+
+            if (ticketNum <= 0) {
+                System.out.println("售票结束...");
+                break;
+            }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("窗口 " + Thread.currentThread().getName() + " 售出一张票"
+                    + " 剩余票数=" + (--ticketNum));//1 - 0 - -1  - -2
+
+        }
+    }
+}
