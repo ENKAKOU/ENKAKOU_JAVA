09b9ef11b648c85b5adb06357f002766b0b5b3c7
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-30 11:17:03 +0900

    Update Debug04.java

--- ENKAKOU_JAVA/git-java/com/hspedu/debug_/Debug04.java
+++ ENKAKOU_JAVA/git-java/com/hspedu/debug_/Debug04.java
@@ -5,9 +5,7 @@ import java.util.Arrays;
 public class Debug04 {
     
     public static void main(String[] args) {
-
         int[] arr = {1, -1, 10, -20 , 100};
-
         Arrays.sort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + "\t");
@@ -20,5 +18,6 @@ public class Debug04 {
         System.out.println("hello500");
         System.out.println("hello600");
         System.out.println("hello700");
+        
     }
 }
