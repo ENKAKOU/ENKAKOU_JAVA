bbc78b2bb0ce2dae38a52a63db82c8c9868a9671
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-30 11:16:23 +0900

    Update Debug01.java

--- ENKAKOU_JAVA/git-java/com/hspedu/debug_/Debug01.java
+++ ENKAKOU_JAVA/git-java/com/hspedu/debug_/Debug01.java
@@ -3,7 +3,6 @@ package com.hspedu.debug_;
 public class Debug01 {
     
     public static void main(String[] args) {
-
         int sum = 0;
         for (int i = 0; i < 5; i++) {
             sum += i;
@@ -11,5 +10,6 @@ public class Debug01 {
             System.out.println("sum=" + i);
         }
         System.out.println("退出for....");
+        
     }
 }
