ea69d30a51611e284821ce6e4f68a444c6b519f4
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-30 11:13:35 +0900

    Update CollectionMethod.java

--- ENKAKOU_JAVA/git-java/com/hspedu/collection_/CollectionMethod.java
+++ ENKAKOU_JAVA/git-java/com/hspedu/collection_/CollectionMethod.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CollectionMethod {
-    @SuppressWarnings({"all"})
+
     public static void main(String[] args) {
         List list = new ArrayList();
 
