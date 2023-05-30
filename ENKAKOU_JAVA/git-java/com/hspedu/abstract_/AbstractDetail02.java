23fe3b750cf400efbdad589395d1d651140bf555
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-30 11:08:48 +0900

    Update AbstractDetail02.java

--- ENKAKOU_JAVA/git-java/com/hspedu/abstract_/AbstractDetail02.java
+++ ENKAKOU_JAVA/git-java/com/hspedu/abstract_/AbstractDetail02.java
@@ -7,16 +7,20 @@ public class AbstractDetail02 {
     }
 }
 
+
 abstract class H {
     public abstract void h1();
 }
+
 
 abstract class E {
     public abstract void h1();
 }
 
+
 abstract class F extends E {
 }
+
 
 class G extends E {
     public void h1() {
@@ -24,14 +28,17 @@ class G extends E {
     }
 }
 
+
 abstract class D {
     public int n1 = 10;
     public static String name = "HSP教育";
     public void h1() {
         System.out.println("h1");
     }
+    
     public abstract void hello();
     public static void ok() {
         System.out.println("ok");
+        
     }
 }
