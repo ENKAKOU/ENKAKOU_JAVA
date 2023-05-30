ef1701b4c85f2fead9856366b42e87e536bdd019
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-30 11:20:51 +0900

    Update NumberFormatException_.java

--- ENKAKOU_JAVA/git-java/com/hspedu/exception_/NumberFormatException_.java
+++ ENKAKOU_JAVA/git-java/com/hspedu/exception_/NumberFormatException_.java
@@ -4,8 +4,8 @@ public class NumberFormatException_ {
 
     public static void main(String[] args) {
         String name = "韩顺平教育";
-
         int num = Integer.parseInt(name);    //抛出NumberFormatException
         System.out.println(num);
+        
     }
 }
