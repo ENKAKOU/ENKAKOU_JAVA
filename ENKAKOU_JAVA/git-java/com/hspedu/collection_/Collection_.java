e02a3713d6e2c098a9af38834c08edb3a8512c24
ENKAKOU <121547866+ENKAKOU@users.noreply.github.com>
2023-05-30 11:13:47 +0900

    Update Collection_.java

--- ENKAKOU_JAVA/git-java/com/hspedu/collection_/Collection_.java
+++ ENKAKOU_JAVA/git-java/com/hspedu/collection_/Collection_.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Collection_ {
-    @SuppressWarnings({"all"})
+
     public static void main(String[] args) {
 
     ArrayList arrayList = new ArrayList();
