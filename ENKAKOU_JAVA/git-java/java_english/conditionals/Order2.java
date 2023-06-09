d4ed44ba78be45e1447ae91e966582d65d74cf0e
enkakou <enkakou0716@gmail.com>
2023-06-09 13:39:09 +0900

    java_english.conditionals

---
+++ ENKAKOU_JAVA/git-java/java_english/conditionals/Order2.java
@@ -0,0 +1,12 @@
+package java_english.conditionals;
+
+public class Order2 {
+
+    public static void main(String[] args) {
+
+        boolean isFilled = false;
+
+        // Write an if-then-else statement:
+
+    }
+}
