50894f0c358225d0833b31f3b213e1a934c1b91f
enkakou <enkakou0716@gmail.com>
2023-06-09 15:02:06 +0900

    string_methods

---
+++ ENKAKOU_JAVA/git-java/java_english/string_methods/HelloWorld.java
@@ -0,0 +1,14 @@
+package java_english.string_methods;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        String str = "Hello, World!";
+
+        System.out.println(str.length());
+        System.out.println(str.substring(4));
+        System.out.println(str.toUpperCase());
+
+    }
+}
---
+++ ENKAKOU_JAVA/git-java/java_english/string_methods/Password.java
@@ -0,0 +1,11 @@
+package java_english.string_methods;
+
+public class Password {
+
+    public static void main(String[] args) {
+
+        String password = "correcthorsebatterystaple";
+        System.out.println(password.equals("Tr0ub4dor&3"));
+
+    }
+}
