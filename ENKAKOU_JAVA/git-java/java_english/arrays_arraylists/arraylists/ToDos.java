6eecbe5cd94274b24e794b0f0385c9cb96b8af89
enkakou <enkakou0716@gmail.com>
2023-06-09 15:01:27 +0900

    arrays_arraylists

---
+++ ENKAKOU_JAVA/git-java/java_english/arrays_arraylists/arraylists/ToDos.java
@@ -0,0 +1,12 @@
+package java_english.arrays_arraylists.arraylists;
+
+import java.util.ArrayList;
+
+class ToDos {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> toDoList = new ArrayList<String>();
+
+    }
+}
