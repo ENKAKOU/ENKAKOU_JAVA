03ddd9c01eab2c7c874c95cded948c4da1331fba
enkakou <enkakou0716@gmail.com>
2023-06-09 17:01:47 +0900

    debugging

---
+++ ENKAKOU_JAVA/git-java/java_english/debugging/Debug.java
@@ -0,0 +1,8 @@
+package java_english.debugging;
+
+public class Debug {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ENKAKOU_JAVA/git-java/java_english/debugging/Debug2.java
@@ -0,0 +1,16 @@
+package java_english.debugging;
+
+public class Debug2 {
+
+    public static void main(String[] args) {
+
+        int steps = 10;
+
+        for (int i = 0; i <= steps; i++) {
+
+            System.out.println("Step #" + i);
+
+        }
+
+    }
+}
