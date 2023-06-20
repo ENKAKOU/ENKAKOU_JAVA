ff670930d031ec33e39a7d7f3d64f15626b23726
enkakou <enkakou0716@gmail.com>
2023-06-20 20:06:10 +0900

    regexp

---
+++ ENKAKOU_JAVA/git-java/chapter27_regexp/RegTheory.java
@@ -0,0 +1,39 @@
+package chapter27_regexp;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegTheory {
+
+    public static void main(String[] args) {
+
+        String content = "1998年12月8日，第二代Java平台的企业版J2EE发布。1999年6月，Sun公司发布了" +
+                "第二代Java平台（简称为Java2）的3个版本：J2ME（Java2 Micro Edition，Java2平台的微型" +
+                "版），应用于移动、无线及有限资源的环境；J2SE（Java 2 Standard Edition，Java 2平台的" +
+                "标准版），应用于桌面环境；J2EE（Java 2Enterprise Edition，Java 2平台的企业版），应" +
+                "用3443于基于Java的应用服务器。Java 2平台的发布，是Java发展过程中最重要的一个" +
+                "里程碑，标志着Java的应用开始普及9889 ";
+
+        //目标：匹配所有四个数字
+        //1.\\d,表示一个任意的数字
+        String regStr = "(\\d\\d)(\\d\\d)";
+        //2.创建模式对象（正则表达式）
+        Pattern pattern = Pattern.compile(regStr);
+        //3.创建匹配器
+        //匹配器matcher，按照”正则表达式“的规则，去匹配content字符串
+        Matcher matcher = pattern.matcher(content);
+
+        //4.开始匹配
+        while (matcher.find()) {
+            //1. 如果正则表达式有() 即分组, 取出匹配的字符串规则
+            //2. group(0) 表示匹配到的子字符串
+            //3. group(1) 表示匹配到的子字符串的第一组字串
+            //4. .group(2) 表示匹配到的子字符串的第2组字串
+            //5. 但是分组的数不能越界
+            System.out.println("找到: " + matcher.group(0));
+            System.out.println("第1组()匹配到的值=" + matcher.group(1));
+            System.out.println("第2组()匹配到的值=" + matcher.group(2));
+
+        }
+    }
+}
---
+++ ENKAKOU_JAVA/git-java/chapter27_regexp/Test.java
@@ -0,0 +1,11 @@
+package chapter27_regexp;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        String str = "abcdef";
+        System.out.println(str.substring(0,4));
+
+    }
+}
